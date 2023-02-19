7f9f2fca94340425fea9a4be1e47f3ce38630250
siddharthav45 <siddhartha.v21@iiits.in>
2023-02-19 11:34:32 +0530

    add sec headers

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const xss = require("xss-clean");
+const expressMongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 // middleware
-app.use(express.static(__dirname + '/public/css'));
-app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(__dirname + "/public/css"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('view engine','ejs');
+app.use(helmet());
+app.use(xss());
+app.use(expressMongoSanitize());
+app.set("view engine", "ejs");
 
 // main
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-mongoose.connect(`mongodb+srv://lancify:${process.env.mongopassword}@cluster0.hripjgl.mongodb.net/?retryWrites=true&w=majority`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    app.listen(5500,()=>{
-        console.log('server started in port 5500');
-    })
-})
+mongoose
+  .connect(
+    `mongodb+srv://lancify:${process.env.mongopassword}@cluster0.hripjgl.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(5500, () => {
+      console.log("server started in port 5500");
+    });
+  });
